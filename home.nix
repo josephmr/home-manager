23d4c7f6847a03938703f0576e0c620b1a2310a8
Joseph Rollins <joseph@guilded.gg>
2023-03-23 10:54:36 -0700

    Add zsh and tools

--- home.nix
+++ home.nix
@@ -14,7 +14,71 @@
     httpie
     jq
     wget
+    bat
+    exa
+    delta
+    du-dust
+    duf
+    choose
+    glances
   ];
+
+  programs.zsh = {
+    enable = true;
+    dotDir = ".config/zsh";
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "";
+    };
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
+      }
+      {
+        name = "pure";
+        src = pkgs.fetchFromGitHub {
+          owner = "sindresorhus";
+          repo = "pure";
+          rev = "v1.21.0";
+          hash = "sha256-YfasTKCABvMtncrfoWR1Su9QxzCqPED18/BTXaJHttg=";
+        };
+      }
+    ];
+    history = {
+      expireDuplicatesFirst = true;
+      ignoreDups = true;
+      save = 40000;
+      size = 40000;
+      share = true;
+    };
+    shellAliases = {
+      emacsd = "emacs --daemon";
+      emacsc = "emacsclient -nc";
+      cat = "bat";
+    };
+    initExtra = ''
+      autoload -U promptinit; promptinit
+      prompt pure
+
+      # Set the title of tmux panes to the running command entered.
+      # This differes from pane_current_command as that outputs the running command name
+      # without arguments. Which also follows aliases (e.g. alias foo=node) would show node
+      # not foo.
+      function set_tmux_title() {
+        if [ "$TERM" != "screen-256color" ]; then
+          return
+        fi
+
+        printf '\033]2;%s\033\\' "$1"
+      }
+
+      preexec_functions+=(set_tmux_title)
+    '';
+  };
 
   programs.tmux = {
     enable = true;
@@ -31,6 +95,19 @@
       set -g pane-border-status top
       set -g pane-active-border-style "bg=#28E945"
     '';
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    defaultCommand = "rg --files --hidden";
+    fileWidgetCommand = "rg --files --hidden";
   };
 
   programs.git = {
@@ -52,7 +129,16 @@
       };
       github.user = "josephmr";
       core.excludesfile = "${config.home.homeDirectory}/.gitignore";
-      core.pager = "less -FRX";
+
+      # Use delta for diffs
+      core.pager = "delta";
+      interactive.diffFilter = "delta --color-only";
+      delta = {
+        navigate = true;
+        light = false;
+      };
+      merge.conflictstyle = "diff3";
+      diff.colorMoved = "default";
     };
   };
 }
