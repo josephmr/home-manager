087df2b2a3a3b4b79a7e3a735718ac286c08766d
Joseph Rollins <joseph@guilded.gg>
2023-03-22 21:29:26 -0700

    Initial home manager config

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-22.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ...}:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations.jrollins = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,57 @@
+{ pkgs, ... }:
+
+{
+  home.username = "jrollins";
+  home.homeDirectory = "/Users/jrollins";
+
+  home.stateVersion = "22.11";
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    overmind
+    ripgrep
+    httpie
+    jq
+    wget
+  ];
+
+  programs.tmux = {
+    enable = true;
+
+    keyMode = "vi";
+    newSession = true;
+    terminal = "screen-256color";
+
+    plugins = with pkgs; [
+      tmuxPlugins.vim-tmux-navigator
+    ];
+
+    extraConfig = ''
+      set -g pane-border-status top
+      set -g pane-active-border-style "bg=#28E945"
+    '';
+  };
+
+  programs.git = {
+    enable = true;
+
+    userEmail = "joseph@guilded.gg";
+    userName = "Joseph Rollins";
+
+    aliases = {
+      delete-squashed = "!f() { local targetBranch=\${1:-master} && git checkout -q $targetBranch && git branch --merged | grep -v \"\\*\" | xargs -n 1 git branch -d && git for-each-ref refs/heads/ \"--format=%(refname:short)\" | while read branch; do mergeBase=$(git merge-base $targetBranch $branch) && [[ $(git cherry $targetBranch $(git commit-tree $(git rev-parse $branch^{tree}) -p $mergeBase -m _)) == \"-\"* ]] && git branch -D $branch; done; }; f";
+      echo-squashed = "!f() { local targetBranch=\${1:-master} && git checkout -q $targetBranch && git branch --merged | grep -v \"\\*\" | xargs -n 1 git branch -d && git for-each-ref refs/heads/ \"--format=%(refname:short)\" | while read branch; do mergeBase=$(git merge-base $targetBranch $branch) && [[ $(git cherry $targetBranch $(git commit-tree $(git rev-parse $branch^{tree}) -p $mergeBase -m _)) == \"-\"* ]] && echo git branch -D $branch; done; }; f";
+    };
+
+    extraConfig = {
+      url = {
+        "ssh://git@github.com/" = {
+          insteadOf = "https://github.com/";
+        };
+      };
+      github.user = "josephmr";
+      core.excludesfile = "~/.gitignore";
+    };
+  };
+}
