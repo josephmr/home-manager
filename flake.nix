72938d12a9085e11e991c1cc194c6d8d3447d6fe
Joseph Rollins <rollins.joseph@gmail.com>
2023-07-03 11:30:26 -0700

    Changes for WSL

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -20,12 +20,12 @@
         modules = [ ./common.nix ./hosts/mbp.nix ];
       };
 
-      "fortruce@weasl" = home-manager.lib.homeManagerConfiguration {
+      "joseph@gaming" = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
           system = "x86_64-linux";
           config.allowUnfree = true;
         };
-        modules = [ ./common.nix ./hosts/weasl.nix ];
+        modules = [ ./common.nix ./hosts/gaming.nix ];
       };
 
       "joseph@starship" = home-manager.lib.homeManagerConfiguration {
